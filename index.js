6e336f3ed8c2d81cfd6876a2616751776663f8d6
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-10-25 08:50:58 +0300

    support for latest opemtmi server
    * upgrade to support winston 3x
    * use opentmi-addon
    * reject when cluster mode active - support only non clustered

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // native modules
 const util = require('util');
-const spawn = require('child_process').spawn;
+const {spawn} = require('child_process');
+const cluster = require('cluster');
+
 // 3rd party modules
 const express = require('express');
 const winston = require('winston');
+const Transport = require('winston-transport');
 const superagent = require('superagent');
 const _ = require('lodash');
+const {Addon} = require('opentmi-addon');
 // application modules
 const logger = require('../../tools/logger');
 
-class BogeyLogger extends winston.Transport {
-  constructor(options) {
-    super({});
+class BogeyLogger extends Transport {
+  constructor(...args) {
+    super(...args);
     this.name = 'BogeyLogger';
     this.level = 'info';
   }
-  log(level, msg, meta, callback) {
-    const statusCode = _.get(meta, 'res.statusCode');
-    const ip = _.get(meta, 'req.headers.host');
-    const uri = _.get(meta, 'req.url');
+  log({metadata}, callback = () => {}) {
+    const statusCode = _.get(metadata, 'res.statusCode');
+    const ip = _.get(metadata, 'req.headers.host');
+    const uri = _.get(metadata, 'req.url');
     if(statusCode && ip && uri) {
       const data = {statusCode, ip, uri};
       superagent.post(`http://localhost:8008/log`)
@@ -28,17 +32,22 @@ class BogeyLogger extends winston.Transport {
     callback(null, true);
   }
 }
-winston.transports.BogeyLogger = BogeyLogger;
 
-class AddonCore {
-  constructor(app, server, io) {
+class AddonRestVisualize extends Addon {
+  constructor(...args) {
+    super(...args);
+    this._bogeyLogger = undefined;
     this.router = express.Router();
+    if (!cluster.isMaster) {
+      throw new Error('Does not support cluster mode');
+    }
   }
   register() {
     logger.warn('registering');
-    winston.add(winston.transports.BogeyLogger)
+    this._bogeyLogger = new BogeyLogger({format: winston.format.json()});
+    this.logger.logger.add(this._bogeyLogger);
     this._bogey = spawn('bogey');
-    this._bogey.on('exit', (code) => {
+    this._bogey.once('exit', (code) => {
       logger.silly('bogey exited');
     });
   }
@@ -49,4 +58,4 @@ class AddonCore {
 }
 
 
-module.exports = AddonCore;
+module.exports = AddonRestVisualize;
